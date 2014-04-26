2e0795bdea16934d781b296de2cacaf899aacff7
Kenneth Uy <missingno15@gmail.com>
2014-04-26 16:14:53 -0400

    created new seed default deck

--- ankiripoff/db/seeds.rb
+++ ankiripoff/db/seeds.rb
@@ -1,6 +1,10 @@
-cards = IO.readlines("./db/dictionary.txt")
-cards.each do |line|
-  words = line.split
-  Card.create(question: words[0], answer: words[1])
+require 'csv'
+
+csvarray = CSV.read('./db/N5-vocab-list-10.csv')
+
+jpn_deck = Deck.create(topic: "Japanese N5 Vocab List")
+
+csvarray.each do |card|
+  Card.create(question: card[0], answer: card[2], decks_id: jpn_deck.id)
 end
 

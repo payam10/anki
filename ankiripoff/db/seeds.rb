002aec03d7e9689e4262260d879aa216c69dcce1
Kenneth Bruce Uy <missingno15@gmail.com>
2014-04-27 14:07:44 -0400

    Made seed data smaller for easier testing

--- ankiripoff/db/seeds.rb
+++ ankiripoff/db/seeds.rb
@@ -1,10 +1,11 @@
 require 'csv'
 
 csvarray = CSV.read('./db/N5-vocab-list-10.csv')
+small_deck = csvarray.first(10)
 
-jpn_deck = Deck.create(topic: "Japanese N5 Vocab List")
+jpn_deck = Deck.create(topic: "Japanese N5 Vocab List", card_count: small_deck.size)
 
-csvarray.each do |card|
-  Card.create(question: card[0], answer: card[2], decks_id: jpn_deck.id)
+small_deck.each do |card|
+  Card.create(question: card[2], answer: card[0], decks_id: jpn_deck.id)
 end
 
